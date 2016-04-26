8dd3ea13432eeefdf12d9d2bfa2d0b2b48db09ca
Dongqing Hu <sorra@outlook.com>
2016-04-26 10:26:32 +0800

    Timing

--- src/main/java/sorra/answerer/Main.java
+++ src/main/java/sorra/answerer/Main.java
@@ -6,21 +6,27 @@ import sorra.answerer.util.CommandProperties;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    if (args.length == 0) {
-      throw new IllegalArgumentException("Require a command");
-    }
-    String command = args[0];
-    CommandProperties commandProperties = new CommandProperties("config.properties");
-    String projectName = commandProperties.getProperty("project.name");
-    String projectPath = commandProperties.getProperty("project.path", projectName);
-    String basePackage = commandProperties.getProperty("base.package");
-    String javaSubDir = commandProperties.getProperty("java.subdir", "src/main/java");
+    long timeStart = System.currentTimeMillis();
+    try {
+      if (args.length == 0) {
+        throw new IllegalArgumentException("Require a command");
+      }
+      String command = args[0];
+      CommandProperties commandProperties = new CommandProperties("config.properties");
+      String projectName = commandProperties.getProperty("project.name");
+      String projectPath = commandProperties.getProperty("project.path", projectName);
+      String basePackage = commandProperties.getProperty("base.package");
+      String javaSubDir = commandProperties.getProperty("java.subdir", "src/main/java");
 
-    ProjectGenerator.init(projectPath, projectName, basePackage, javaSubDir);
-    if (command.equals("create")) {
-      ProjectGenerator.create();
-    } else if (command.equals("update")) {
-      DoWire.run(projectPath, javaSubDir);
+      ProjectGenerator.init(projectPath, projectName, basePackage, javaSubDir);
+      if (command.equals("create")) {
+        ProjectGenerator.create();
+      } else if (command.equals("update")) {
+        DoWire.run(projectPath, javaSubDir);
+      }
+    } finally {
+      long timeCost = System.currentTimeMillis() - timeStart;
+      System.out.printf("Answerer time cost: %d.%d seconds\n", timeCost / 1000, timeCost % 1000);
     }
   }
 }
